7cf8e400c4ad1f5319ab54caf2365bb29dcbdf6f
Hunter Evanoff <hunter547@gmail.com>
2022-05-05 19:58:06 -0600

    Add the gas station problem.

---
+++ python-solutions/gas-station.py
@@ -0,0 +1,16 @@
+# Keep in mind that only one solution exists. Therefore, if the cost to get to the next station
+# is greater than the gas able to be received at the current station plus any gas in the tank, then
+# we can safely assume that the current index can't be the starting point and we can move the
+# start_station pointer to the next gas statiton in the list.
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_tank, start_station, curr_tank = 0, 0, 0
+
+        for i in range(len(gas)):
+            curr_tank += gas[i] - cost[i]
+            total_tank += gas[i] - cost[i]
+            if (curr_tank < 0):
+                curr_tank = 0
+                start_station = i + 1
+
+        return start_station if total_tank >= 0 else -1
